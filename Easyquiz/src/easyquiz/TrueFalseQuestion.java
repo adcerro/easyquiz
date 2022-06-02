3b44d22015a5cba3b75e53cfe8b8e18912b73d41
adcerro <adcerro@uninorte.edu.co>
2022-06-01 21:57:50 -0500

    new: commented code on base clases

--- Easyquiz/src/easyquiz/TrueFalseQuestion.java
+++ Easyquiz/src/easyquiz/TrueFalseQuestion.java
@@ -12,13 +12,21 @@ import java.util.Arrays;
  * @author aland
  */
 public class TrueFalseQuestion extends Question {
-    private static ArrayList<String> options = new ArrayList<>(Arrays.asList(new String[] {
+
+    private static ArrayList<String> options = new ArrayList<>(Arrays.asList(new String[]{
         "Verdadero",
         "Falso"
     }));
-    
+
     private boolean ans;
 
+    /**
+     * Creates a true or false question
+     * <p>
+     * 
+     * @param quest  The question
+     * @param ans The answer
+     */
     TrueFalseQuestion(String quest, boolean ans) {
         super(quest, options, ans ? 0 : 1);
         this.ans = ans;
